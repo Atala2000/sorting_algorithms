16ce7b5ea5f041c3a0b4497abdb2ea76c1cde22b
Atala2000 <atalakidi@gmail.com>
2023-06-27 07:59:17 -0700

    1 2 and 3

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,34 +1,34 @@
 #include "sort.h"
 
 /**
- * bubble_sort - this function sorts an array
- * @array: array that is of integers
- * @size: size of arr
- * 
- * 
+* bubble_sort - this function sorts an array
+* @array: array that is of integers
+* @size: size of arr
+*
+*
 */
 void bubble_sort(int *array, size_t size)
 {
-    int tmp;
-    size_t i, j;
+int tmp;
+size_t i, j;
 
-    if (!array || !size)
-            return;
-    
-    i = 0;
-    while(i < size)
-    {
-        for (j = 0; j < size - 1; j++)
-        {
-           if (array[j] > array[j + 1])
-           {
-            tmp = array[j];
-            array[j] = array[j + 1];
-            array[j + 1] = tmp;
-            print_array(array, size);
-           } /* code */
-        }
-        i++;
-    }
-        
+if (!array || !size)
+return;
+
+i = 0;
+while (i < size)
+{
+for (j = 0; j < size - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+tmp = array[j];
+array[j] = array[j + 1];
+array[j + 1] = tmp;
+print_array(array, size);
+} /* code */
+}
+i++;
+}
+
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,35 @@
 #include "sort.h"
 
 /**
- * selection_sort - selecion sort
- * @array: array
- * @size: size
+* selection_sort - selecion sort
+* @array: array
+* @size: size
 */
 void selection_sort(int *array, size_t size)
 {
-    size_t a, b, c;
-    int tmp;
+size_t a, b, c;
+int tmp;
 
-    if (!array || !size)
-    {
-        return;
-    }
-    for (a = 0; a < size - 1; a++)
-    {
-        for (b = size - 1, c = a + 1; b > a; b--)
-        {
-            if (array[b] < array[c])
-            {
-                c = b;
-            }
-        }
-        if (array[a] > array[c])
-        {
-            tmp = array[a];
-            array[a] = array[c];
-            array[c] = tmp;
-            print_array(array, size);
-        }
-    }
-    
+if (!array || !size)
+{
+return;
+}
+for (a = 0; a < size - 1; a++)
+{
+for (b = size - 1, c = a + 1; b > a; b--)
+{
+if (array[b] < array[c])
+{
+c = b;
+}
+}
+if (array[a] > array[c])
+{
+tmp = array[a];
+array[a] = array[c];
+array[c] = tmp;
+print_array(array, size);
+}
+}
+
 }
