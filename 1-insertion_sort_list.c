910688f344bbb7f43872db7d71cfbd4154fc1a5c
Atala2000 <111738703+Atala2000@users.noreply.github.com>
2023-06-27 17:45:52 +0300

    Add files via upload

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,62 @@
+#include "sort.h"
+
+/**
+ * swap - swaps nodes
+ * @a: pointer to 1st node
+ * @b: second node
+ * Return : Nothing
+*/
+void swap(listint_t *a, listint_t *b)
+{
+    if (a->prev)
+    {
+        a->prev->next = b;
+    }
+    if (b->next)
+    {
+        b->next->prev = a;
+    }
+    a->next = b->next;
+    b->prev = a->prev;
+    a->prev = b;
+    b->next = a;
+}
+/**
+ * insertion_sort_list - insertion sort
+ * @list: Pointer to head node
+ * Return: Nothing
+*/
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *a, *b;
+
+    if (!list || !*list || !(*list)->next)
+    {
+        return;
+    }
+
+    a  = (*list)->next;
+    while (a)/* condition */
+    {
+        b = a;
+        a = a->next;
+        while (b && b->prev)
+        {
+           if (b->prev->n > b->n)
+           {
+            swap(b->prev, b);
+            if (!b->prev)
+            {
+                *list = b;
+            }
+            print_list((const listint_t *)*list);
+           }
+           else
+           {
+            b = b->prev;
+           }
+        }
+        
+    }
+    
+}
---
+++ 3-quick_sort.c
@@ -0,0 +1,74 @@
+#include "sort.h"
+
+/**
+ * swap - swaps nodes
+ * @a: pointer to 1st node
+ * @b: second node
+ * Return: Nothing
+*/
+void swap(int *array, size_t size, int *a, int *b)
+{
+    if (*a != *b)
+    {
+        *a = *a + *b;
+        *b = *a - *b;
+        *a = *a - *b;
+        print_array((const int *)array, size);
+    }
+}
+
+/**
+ * lomuto_part - partition
+ * @array: array
+ * @size: size
+ * @low: low
+ * @high: high
+ * Return: nothing
+*/
+size_t lomuto_part(int *array, size_t size, ssize_t low, ssize_t high)
+{
+    int a, b, piv = array[high];
+
+    for (a = b = low; b < high; b++)
+    {
+        if (array[b] < piv)
+        {
+            swap(array, size, &array[b], &array[a++]);
+        }
+    }
+    swap(array, size, &array[a], &array[high]);
+    return (a);
+}
+
+/**
+ * quick_sort_recursive - recursive function for quicksort
+ * @array: integer
+ * @size: size
+ * @low: low
+ * @high: high
+ * Return: void
+*/
+void quick_sort_recursive(int *array, size_t size, ssize_t low, ssize_t high)
+{
+    if (low < high)
+    {
+        size_t p = lomuto_part(array, size, low, high);
+
+        quick_sort_recursive(array, size, low, p - 1);
+        quick_sort_recursive(array, size, p + 1, high);
+    }
+}
+
+/**
+ * quick_sort - sort
+ * @array: array
+ * @size: size
+ * Return: void
+*/
+void quick_sort(int *array, size_t size)
+{
+    if (array == NULL || size < 2)
+        return;
+
+    quick_sort_recursive(array, size, 0, size - 1);
+}
